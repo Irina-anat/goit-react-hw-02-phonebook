bfe70ee8755164b362f61e7e439898545daeb260
Iryna Rozmaita <iriszechka@ukr.net>
2023-06-10 16:53:29 +0300

    + add contact

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import css from './App.module.css'
 import { nanoid } from "nanoid";
+
 import  ContactForm  from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
 
@@ -13,25 +14,40 @@ export class App extends React.Component {
     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-],
-  filter: ''
-}
+    ],
   
-  contactId = nanoid();
+    filter: ''
+  }
+  
+  addContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    }
+   
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }))
+  };
+
   //відфільтровую контакти і залишаю ті ел. id яких не співпадають
   deleteContact = (contactId) => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }))
   };
+
   
   
   render() {
-    const {contacts} = this.state
+    const { contacts } = this.state;
     return (
       <div className={css.container__phonebook}>
         <h1>Phonebook</h1>
-        <ContactForm id={this.contactId} />
+        <ContactForm 
+          onSubmit={this.addContact}
+        />
         <h2>Contacts</h2>
         <ContactList contacts={contacts}
           onDeleteContact={this.deleteContact} />
@@ -40,4 +56,51 @@ export class App extends React.Component {
     )}
 }
 
+
+
+
+
+
+
+/*export class App extends React.Component {
+
+  state = {
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+],
+  filter: ''
+}
+  
+  addContact = text => {
+   console.log(text)
+ }
+  //відфільтровую контакти і залишаю ті ел. id яких не співпадають
+  deleteContact = (contactId) => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }))
+  };
+
+//this.props.onSubmit(this.state) ContactForm - дані при відправці ф-ми
+  formSubmitHandler = data => {
+  console.log(data)
+}
+   
+  render() {
+    const {contacts} = this.state
+    return (
+      <div className={css.container__phonebook}>
+        <h1>Phonebook</h1>
+        <ContactForm 
+          onSubmit={this.formSubmitHandler}
+        />
+        <h2>Contacts</h2>
+        <ContactList contacts={contacts}
+          onDeleteContact={this.deleteContact} />
+      </div>
+    )}
+}*/
 
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,35 +1,145 @@
 import React from 'react'
 import css from './ContactForm.module.css'
+import { nanoid } from "nanoid";
 
- class ContactForm extends React.Component{
+class ContactForm extends React.Component{
     state = {
- name: ''
- }
+        name: '',
+        number: ''
+     }
+     
+    handleChange = event => {
+    const { name, value } = event.currentTarget;
+        this.setState({[name]: value,})
+    }
 
+    //onSubmit={this.formSubmitHandler}
+    handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state); 
+    this.setState({
+      name: '',
+      number: '',
+    });
+        this.reset();    
+    }
+
+    inputNameId = nanoid();
+    inputNumberId =  nanoid();
+    
+    //oчищення форми після submit
+    reset = () => {
+        this.setState({ name: ' ', number: ' ' })
+    };
 
     render() {
-        const { id } = this.props;
-        return (<div>
-            <form className={css.form}>
-    <label>
+    
+    const { name, number } = this.state;
+
+    return (<div>
+    <form className={css.form}
+        onSubmit={this.handleSubmit}>
+    <label htmlFor={this.inputNameId}>
         Name
         <input 
-        type="text"
-        name="name"
-        id={id}
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
+    type="text"
+    name="name"
+    pattern="^[a-zA-Zа-яА-Я\s]+$" 
+    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    required
+        value={name}
+        onChange={this.handleChange}
+        id={this.inputNameId}
         />
     </label>
           
-    <label>
+    <label htmlFor={this.inputNumberId}>
        Number
        <input type="tel"
-       name="number"
-       pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-       title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-       required />
+    name="number"
+    pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+    required
+        value={number}
+        onChange={this.handleChange}
+        id={this.inputNumberId}
+        />
+    </label>
+
+    <button type="submit">Add contact</button>
+        </form>
+           </div>  
+       )}
+};
+
+export default ContactForm;
+
+
+
+
+
+
+
+
+/*class ContactForm extends React.Component{
+    state = {
+        name: '',
+        number: ''
+     }
+     
+    handleChange = event => {
+    const { name, value } = event.currentTarget;
+        this.setState({[name]: value,})
+    }
+
+    //onSubmit={this.formSubmitHandler}
+    handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state); 
+        this.reset();    
+    }
+
+    inputNameId = nanoid();
+    inputNumberId =  nanoid();
+    
+    //oчищення форми після submit
+    reset = () => {
+        this.setState({name: ' ',number: ' '})
+    }
+
+    render() {
+    
+    const { name, number } = this.state;
+
+    return (<div>
+    <form className={css.form}
+        onSubmit={this.handleSubmit}>
+    <label htmlFor={this.inputNameId}>
+        Name
+        <input 
+    type="text"
+    name="name"
+    pattern="^[a-zA-Zа-яА-Я\s]+$" 
+    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    required
+        value={name}
+        onChange={this.handleChange}
+        id={this.inputNameId}
+        />
+    </label>
+          
+    <label htmlFor={this.inputNumberId}>
+       Number
+       <input type="tel"
+    name="number"
+    pattern="\+?[0-9\s\-\(\)]+"
+    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+    required
+     
+        value={number}
+        onChange={this.handleChange}
+        id={this.inputNumberId}
+        />
     </label>
 
     <button type="submit">Add contact</button>
@@ -37,6 +147,4 @@ import css from './ContactForm.module.css'
            </div>
        
        ) }
-};
-
-export default ContactForm;
+};*/
