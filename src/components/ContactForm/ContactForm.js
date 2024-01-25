c5c4fc8c3dfe13fd76e0d5832eab92ebf14a21c8
Iryna Rozmaita <iriszechka@ukr.net>
2024-01-25 12:02:28 +0200

    update ContactForm

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,85 +1,77 @@
 import React from 'react';
 import css from './ContactForm.module.css';
-import { nanoid } from "nanoid";
+import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 
+class ContactForm extends React.Component {
+  state = {
+    name: '',
+    number: '',
+  };
 
-class ContactForm extends React.Component{
-    state = {
-        name: '',
-        number: ''
-    };
-     
-    handleChange = event => {
-        const { name, value } = event.currentTarget;
-        this.setState({ [name]: value, })
-    };
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
 
-    //onSubmit={this.formSubmitHandler}   onSubmit={this.addContact}
-    handleSubmit = event => {
-        event.preventDefault();
-        this.props.onSubmit(this.state);
-        this.setState({
-            name: '',
-            number: '',
-        });
-       // this.reset();
-    };
+  //onSubmit={this.formSubmitHandler}   onSubmit={this.addContact}
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
 
-    inputNameId = nanoid();
-    inputNumberId =  nanoid();
-    
-    //oчищення форми після submit
-    reset = () => {
-        this.setState({ name: ' ', number: ' ' })
-    };
+  inputNameId = nanoid();
+  inputNumberId = nanoid();
 
-    render() {
+  //oчищення форми після submit
+  reset = () => {
+    this.setState({ name: ' ', number: ' ' });
+  };
+
+  render() {
     const { name, number } = this.state;
-    return (<div>
-    <form className={css.form}
-        onSubmit={this.handleSubmit}>
-    <label htmlFor={this.inputNameId}>
-        Name
-        <input 
-    type="text"
-    name="name"
-    pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$" 
-    title="Name may contain only  letters, spaces, hyphens, and apostrophes are allowed"
-    required
-        value={name}
-        onChange={this.handleChange}
-        id={this.inputNameId}
-        />
-    </label>
-          
-    <label htmlFor={this.inputNumberId}>
-       Number
-       <input type="tel"
-    name="number"
-    pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-    title="Valid Phone Number: Optional '+' Symbol, Digits, Spaces, Hyphens, and Parentheses"
-    required
-        value={number}
-        onChange={this.handleChange}
-        id={this.inputNumberId}
-        />
-    </label>
-
-    <button type="submit">Add contact</button>
+    return (
+      <div>
+        <form className={css.form} onSubmit={this.handleSubmit}>
+          <label htmlFor={this.inputNameId}>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only  letters, spaces, hyphens, and apostrophes are allowed"
+              required
+              value={name}
+              onChange={this.handleChange}
+              id={this.inputNameId}
+            />
+          </label>
+          <label htmlFor={this.inputNumberId}>
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+              title="Valid Phone Number: Optional '+' Symbol, Digits, Spaces, Hyphens, and Parentheses"
+              required
+              value={number}
+              onChange={this.handleChange}
+              id={this.inputNumberId}
+            />
+          </label>
+          <button type="submit">Add contact</button>
         </form>
-           </div>  
-       )}
-};
-
-ContactForm.propTypes = {
-    onSubmit: PropTypes.func.isRequired,
+      </div>
+    );
+  }
 }
 
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
 export default ContactForm;
-
-
-
 
 
 
